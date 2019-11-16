036d0e6dda871cd8b417f8e479804ee14967f1d0
Malte Brodmann <mbrodmann@student.ethz.ch>
2019-11-16 20:15:33 +0100

    extended key-value-store functionality and implemented initial netcache client api

---
+++ src/kv_store/client_api.py
@@ -0,0 +1,51 @@
+import socket
+
+def build_message(op, key, seq=0, value = None):
+
+    msg = bytearray()
+    msg += op.to_bytes(1, 'big')
+    msg += seq.to_bytes(4, 'big')
+    msg += key.to_bytes(16, 'big')
+
+    if value != None:
+        msg += bytes(value, "utf-8")
+
+    return msg
+
+class NetCacheClient:
+
+    def __init__(self, host,port):
+        self.host = host
+        self.port = port
+        self.udps = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def read(self, key, seq=0):
+        msg = build_message(0, key, seq)
+        self.udps.connect((self.host, self.port))
+        self.udps.send(msg)
+        data = self.udps.recvfrom(1024)
+        self.udps.close
+        print(data[0].decode("utf-8"))
+
+    def put(self, key, value, seq = 0):
+        msg = build_message(1, key, seq, value)
+        tcps = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcps.connect((self.host, self.port))
+        tcps.send(msg)
+        tcps.close()
+
+    def delete(self, key, seq = 0):
+        msg = build_message(2, key, seq)
+        tcps = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcps.connect((self.host, self.port))
+        tcps.send(msg)
+        tcps.close()
+
+client = NetCacheClient('127.0.0.1', 40007)
+
+client.read(0)
+client.put(0,"test")
+client.read(1)
+client.read(0)
+client.delete(0)
+client.read(0)
--- src/kv_store/server.py
+++ src/kv_store/server.py
@@ -6,8 +6,6 @@ import sys
 
 import threading
 
-from scapy.all import *
-
 LOGGING_FILE = 'server.log'
 
 logging.basicConfig(
@@ -30,61 +28,107 @@ class KVServer:
         self.host = host
         # port server is listening to
         self.port = port
-        # server socket
-        self.ss = None
+        # udp server socket
+        self.udpss = None
+        #tcp server socket
+        self.tcpss = None
         # max clients to listen to
         self.max_listen = max_listen
 
 
     def activate(self):
-        # create socket server and start listening on specified port
-        self.ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.ss.bind((self.host, self.port))
+
+        # create udp socket server
+        self.udpss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udpss.bind((self.host, self.port))
+
+        # create tcp socket server
+        self.tcpss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcpss.bind((self.host, self.port))
+        self.tcpss.listen(1)
 
         logging.info('Key-Value store server started listening on port ' + str(self.port))
 
+        # spawn new thread that serves incoming udp (read) queries
+        server_udp_t = threading.Thread(target=self.handle_client_udp_request)
+        server_udp_t.start()
+
+        # spawn new thread that serves incoming tcp (put/delete) queries
+        server_tcp_t = threading.Thread(target=self.handle_client_tcp_request)
+        server_tcp_t.start()
+
+
+    # handles incoming udp queries (i.e. read queries)
+    def handle_client_udp_request(self):
+
         while True:
-            # spawn new thread for each incoming connection
-            #server_t = threading.Thread(target=self.handle_client_request)
-            #server_t.setDaemon(True)
-            #server_t.start()
-            self.handle_client_request()
 
+            # netcache_pkt is an array of bytes belonging to incoming packet's data
+            # the data portion of the packet represents the netcache header, so we
+            # can extract all the fields defined in the netcache custom protocol
 
-    def handle_client_request(self):
-        # netcache_pkt is an array of bytes belonging to incoming packet's data
-        # the data portion of the packet represents the netcache header, so we
-        # can extract all the fields defined in the netcache custom protocol
-        netcache_pkt, addr = self.ss.recvfrom(1024)
-        op = netcache_pkt[0]
-        seq = netcache_pk[1:5]
-        key = netcache_pkt[5:21]
+            netcache_pkt, addr = self.udpss.recvfrom(1024)
 
-        if key == NETCACHE_READ_QUERY:
-            cache_read(key)
-        elif key == NETCACHE_WRITE_QUERY:
-            val = netcache_pkt[21:]
-            cache_write(key, val)
-        elif key == NETCACHE_DELETE_QUERY:
-            cache_delete(key)
-        else
-            logging.info('Unsupported query type received from client ' + addr)
+            op = netcache_pkt[0]
+            seq = netcache_pkt[1:5]
+            key = netcache_pkt[5:21]
+            value = netcache_pkt[21:]
 
+            #transform key to int
+            key = int.from_bytes(key,'big')
 
-    # write value for a given key to netcache
-    def cache_write(self, key, value):
-        pass
+            if op != NETCACHE_READ_QUERY:
+                logging.info('Unsupported/Invalid query type received from client ' + addr[0])
+            else:
+                logging.info('Received READ from client ' + addr[0] + ":" + str(addr[1]))
 
+                if key in self.kv_store:
+                    val = self.kv_store[key]
+                    self.udpss.sendto(bytes(val, "utf-8"), addr)
+                else:
 
-    # read value for a given key from netcache
-    def cache_read(self, key):
-        pass
+                    # TODO: ?what is the behaviour in this case?
+                    self.udpss.sendto(bytes("no such key", "utf-8"), addr)
 
+    # serves incoming tcp queries (i.e. put/delete)
+    def handle_client_tcp_request(self):
 
-    # delete value for a given key from netcache
-    def cache_delete(self, key):
-        pass
+        while True:
 
+            conn, addr = self.tcpss.accept()
+
+            netcache_pkt = conn.recv(1024)
+
+            op = netcache_pkt[0]
+            seq = netcache_pkt[1:5]
+            key = netcache_pkt[5:21]
+            value = netcache_pkt[21:]
+
+            #transform key to int
+            key = int.from_bytes(key,'big')
+            #transform val to string
+            value = value.decode("utf-8")
+
+            if op == NETCACHE_WRITE_QUERY:
+                logging.info('Received WRITE from client ' + addr[0] + ":" + str(addr[1]))
+
+                #inserts or updates the value of the respective key
+                self.kv_store[key] = value
+
+                # TODO: ?are we supposed to return something?
+                conn.close()
+
+            elif op == NETCACHE_DELETE_QUERY:
+                logging.info('Received DELETE from client ' + addr[0] + ":" + str(addr[1]))
+
+                if key in self.kv_store:
+                    del self.kv_store[key]
+
+                # TODO: ?are we supposed to return something?
+                conn.close()
+
+            else:
+                logging.info('Unsupported query type received from client ' + addr[0] + ":" + str(addr[1]))
 
 server = KVServer('127.0.0.1', 40007)
 server.activate()

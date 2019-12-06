1f5e52ec57840d726b18d84f147330c3d635e725
Dimitris Lekkas <dlekkasp@gmail.com>
2019-12-06 22:57:05 +0000

    Fixed data generation script to account for range-based partioning

--- src/kv_store/produce_keyvals.sh
+++ src/kv_store/produce_keyvals.sh
@@ -26,9 +26,37 @@ do
 done
 
 
+# helper function to convert a character to its
+# ascii representation
+ord() {
+	LC_CTYPE=C printf '%d' "'$1"
+}
+
+
 mkdir -p $data_dir
 
 for i in $(seq $n_servers); do
+
+	# key must start with this char to be assigned to this
+	# server based on the range-based partitioning scheme
+	# that is used to partition data across storage nodes
+	start_char=''
+
+	# since we are using range-based partitioning, we need
+	# to populate the servers with appropriate values that
+	# start with a proper symbol, to find such a symbol we
+	# iterate over the alphabet until out first match
+	for letter in {{a..z},{A..Z}}; do
+
+		node_no=$(($(ord ${letter}) % ${n_servers}))
+
+		if (( $node_no == $i-1 )); then
+			start_char=$letter
+			break
+		fi
+
+	done
+
 
 	file_name="${data_dir}/server${i}.txt"
 
@@ -36,7 +64,7 @@ for i in $(seq $n_servers); do
 		echo "Error: File $file_name already exists."
 	else
 		for j in $(seq $n_values); do
-			echo "s${i}_key${j} = s${i}_val${j}" >> $file_name
+			echo "${start_char}_s${i}_key${j}=s${i}_val${j}" >> $file_name
 		done
 	fi
 
